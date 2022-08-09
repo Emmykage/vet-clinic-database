9fff45ea2ffc3e096835de3651a491164113a042
Menanya <emmmiemenz@gmail.com>
2022-08-09 14:23:00 +0100

    input animal details and search queries

--- data.sql
+++ data.sql
@@ -3,3 +3,11 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+
+
+INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, true, 10.23);                                                                                                    INSERT 0 1            
+
+INSERT INTO animals (name, date_of_birth, escape_attempt, weight_kg, neutered) VALUES ('Gabumon', '2018-11-15', 2, 8, true); 
+INSERT INTO animals (name, date_of_birth, escape_attempt, weight_kg, neutered) VALUES ('Pikachu', '2021-01-7', 1, 15.04, false);   
+
+INSERT INTO animals (name, date_of_birth, escape_attempt, weight_kg, neutered) VALUES ('Devimon', '2017-05-12', 5, 11, true);   
